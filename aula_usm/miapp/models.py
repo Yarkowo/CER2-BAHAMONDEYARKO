00a8e4cbecbe4b32070f5eddf1cfd5f9a0302c47
Yarkowo <yarko.bahamonde@usm.cl>
2023-10-23 15:53:34 -0300

    solo faltan los usuarios (y dejarlo bonito supongo)

--- aula_usm/miapp/models.py
+++ aula_usm/miapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -26,8 +26,8 @@ class Comunicado(models.Model):
     visible = models.BooleanField(default=True)
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     fecha_ultima_modificacion = models.DateTimeField(auto_now=True)
-    #publicado_por = models.User
-    #modificado_por = models.User
+    publicado_por = models.ForeignKey(User, on_delete=models.CASCADE, editable=False, null=True)
+    modificado_por = models.ForeignKey(User, related_name="modificado_por", on_delete=models.CASCADE, editable=False, null=True)
     def __str__(self):
         return self.titulo
 
--- aula_usm/miapp/views.py
+++ aula_usm/miapp/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Entidad, Comunicado
 
 # Create your views here.
 
+#def Combo_select(request): 
+#    respuesta = request.GET.get['CBOdepartamento']
+
+
+
 def index(request):
-    return render(request,'miapp/base.html')
+    title = "Sistema notificaciones usm"
+
+    respuesta = request.GET.get('Departamento')
+
+    entidades = Entidad.objects.all()
+
+    if respuesta == "Departamento" or respuesta is None:
+        comunicados = Comunicado.objects.all()
+    else:
+        filtro = Entidad.objects.get(nombre=respuesta)
+        #logo = Entidad.logo.get(logo=respuesta)
+        print("filtro: ",filtro.logo)
+        comunicados = Comunicado.objects.filter(entidad=filtro)
+        print("comunicados: ",comunicados)
+        
+        
+    
+    data = {
+        "title": title,
+        "entidades":entidades,
+        "comunicados":comunicados,
+        "respuesta":respuesta,
+
+        
+    }
+
+    return render(request,'miapp/base.html', data)
+
+
 
